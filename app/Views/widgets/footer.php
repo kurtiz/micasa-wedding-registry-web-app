30b0a873a346ff802900113a7a90138e7adfb202
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-06-23 17:46:37 +0000

    Completed main touches on home page :lipstick:

---
+++ app/Views/widgets/footer.php
@@ -0,0 +1,88 @@
+<footer>
+    <!-- Footer Middle Start -->
+    <div class="footer-middle">
+        <div class="container">
+            <div class="container-footer ptb-100">
+                <div class="row">
+                    <!-- Single Footer Start -->
+                    <div class="single-footer col-lg-7 col-md-7">
+                        <div class="footer-logo">
+                            <a href="<?=base_url()?>">
+<!--                                <img class="img" src="--><?//=base_url()?><!--/public/img/logo/logo.webp" alt="logo-img">-->
+                                <h2><strong>MiCasa</strong></h2>
+                            </a>
+                        </div>
+                        <div class="footer-content">
+                            <p>We deliver the best quality.</p>
+                            <h5 class="contact-info mtb-10">contact info:</h5>
+                            <ul class="footer-list first-content">
+                                <li><i class="pe-7s-map-marker"></i>Address will be here</li>
+                                <li><i class="pe-7s-mail"></i>your-email@example.com</li>
+                                <li><i class="pe-7s-call"></i>+00 123 45678</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Single Footer Start -->
+
+
+                    <!-- Single Footer Start -->
+                    <div class="single-footer col-lg-5 col-md-5">
+                        <h4 class="footer-title">NEWSLETTER</h4>
+                        <div class="footer-content subscribe-form">
+                            <p class="mb-25">Subscribe to our newsletter and get updated on our discounts</p>
+                            <div class="subscribe-box">
+                                <form action="#">
+                                    <input type="text" name="subscribe_email" id="subscribe_email" placeholder="Enter you email address here...">
+                                    <input type="submit" class="submit" value="subscribe">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Single Footer Start -->
+                </div>
+                <!-- Row End -->
+            </div>
+            <!-- Container Footer End -->
+        </div>
+        <!-- Container End -->
+    </div>
+    <!-- Footer Middle End -->
+    <!-- Footer Bottom Start -->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="container-footer ptb-30">
+                <div class="row">
+                    <div class="col-md-7">
+                        <p class="text-md-start copyright-text">Copyright ©
+                            <a target="_blank" href="<?=base_url()?>" style="color: #e24646">MiCasa</a>
+                            All Rights Reserved.
+                        </p>
+                        <p>
+                            Developed by
+                            <a href="https://aaron.ourtechnologies.org" target="_blank" style="color: #e24646">
+                                Papilio Technologies
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <!-- Footer Social List Start -->
+                        <div class="socila-footer">
+                            <ul class="social-footer-list list-inline text-end">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                        <!-- Footer Social List End -->
+                    </div>
+                </div>
+                <!-- Row End -->
+            </div>
+            <!-- Container Footer End -->
+        </div>
+        <!-- Container End -->
+    </div>
+    <!-- Footer Bottom End -->
+</footer>
---
+++ app/Views/widgets/preloader.php
@@ -0,0 +1,9 @@
+<div class="preloader">
+    <div class="loading-center">
+        <div class="loading-center-absolute">
+            <lottie-player src="<?=base_url()?>/public/animations/lf20_3qe7zc29.json"
+                           background="transparent" speed="1"  style="width: 300px; height: 300px;" hover loop autoplay>
+            </lottie-player>
+        </div>
+    </div>
+</div>
