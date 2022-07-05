1eee6bef42de77ba306a32562f326f7842e90179
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-07-05 16:02:50 +0000

    Updated favicon :lipstick:

--- app/Views/widgets/css.php
+++ app/Views/widgets/css.php
@@ -1,4 +1,4 @@
-<link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>/public/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="<?=base_url()?>/public/img/logo/favicon.svg">
 <!-- mobile menu css -->
 <link rel="stylesheet" href="<?=base_url()?>/public/css/meanmenu.min.css">
 <!-- animate css -->
